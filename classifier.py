949189a8f241d2e7bcb3fcd7131d19fe565d93e7
Jaakko Pasanen <jaakko.o.pasanen@gmail.com>
2016-11-26 23:03:19 +0200

    Fixed predictor

--- classifier.py
+++ classifier.py
@@ -1,6 +1,7 @@
 from datasimulation.TransactionManager import TransactionManager
 from classifier_trainer import get_training_data
 from classification.classify import load_model
+import numpy as np
 
 
 def predict(path_to_user_data):
@@ -16,7 +17,12 @@ def predict(path_to_user_data):
     x_data, y_true = get_training_data(user_transactions=user_transactions)
 
     # Predict classes
-    y_pred = model.predict_classes(x_data, batch_size=17)
+    y_pred = model.predict_classes(x_data.as_matrix(), batch_size=17)
+    real_classes = []
+    for i in y_true:
+        real_classes.append(np.where(i == 1)[0][0])
+    print(list(y_pred))
+    print(real_classes)
     return x_data, y_pred
 
 
